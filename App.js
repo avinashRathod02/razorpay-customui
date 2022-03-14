09c2ac37116aa44a99f5d48ee724b4dc63a90d80
avinashrathod <http.127001.com@gmail.com>
2022-03-14 14:33:30 +0530

    init

---
+++ App.js
@@ -0,0 +1,12 @@
+import {View, Button, TouchableOpacity, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button title="Pay now" />
+    </View>
+  );
+};
+
+export default App;
